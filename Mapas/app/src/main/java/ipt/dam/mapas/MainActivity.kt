d02394731dcf5083e0504734123aa6754005a587
pauloagsantos <pauloagsantos@gmail.com>
2024-11-22 17:31:11 +0000

    versão inicial

---
+++ Mapas/app/src/main/java/ipt/dam/mapas/MainActivity.kt
@@ -0,0 +1,86 @@
+package ipt.dam.mapas
+
+import android.content.pm.PackageManager
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.osmdroid.views.MapView
+import android.Manifest
+import android.os.Handler
+import android.os.Looper
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.CustomZoomButtonsController
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.compass.CompassOverlay
+
+class MainActivity : AppCompatActivity() {
+
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 1
+    private lateinit var map : MapView
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        requestPermissionsIfNecessary(arrayOf(
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_WIFI_STATE,
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ))
+
+        Configuration.getInstance().setUserAgentValue(this.getPackageName())
+        map = findViewById(R.id.map)
+        map.setTileSource(TileSourceFactory.MAPNIK)
+        map.controller.zoomTo(17.0)
+        map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.ALWAYS)
+        map.setMultiTouchControls(true) // para poder fazer zoom com os dedos
+        var compassOverlay = CompassOverlay(this, map)
+        compassOverlay.enableCompass()
+        map.overlays.add(compassOverlay)
+        var point = GeoPoint(39.60068, -8.38967)
+        var startMarker = Marker(map)
+        startMarker.position = point
+        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_CENTER)
+        startMarker.infoWindow = MarkerWindow(map, this)
+        map.overlays.add(startMarker)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            map.controller.setCenter(point)
+        }, 1000) // espera 1 Segundo para centrar o mapa
+
+    } // onCreate
+
+    private fun requestPermissionsIfNecessary(permissions:Array<out String>) {
+        val permissionsToRequest = ArrayList<String>();
+        permissions.forEach { permission ->
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted
+                permissionsToRequest.add(permission);
+            }
+        }
+        if (permissionsToRequest.size > 0) {
+            ActivityCompat.requestPermissions(
+                this,
+                permissionsToRequest.toArray(arrayOf<String>()),
+                REQUEST_PERMISSIONS_REQUEST_CODE);
+        }
+    }
+
+}
